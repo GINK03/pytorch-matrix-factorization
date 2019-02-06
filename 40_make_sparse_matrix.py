6081622ae92c8648cdc49f2e5d0df33a6d478997
gimpei <gim.kobayashi@gmail.com>
2019-02-07 00:00:48 +0900

    update

--- 40_make_sparse_matrix.py
+++ 40_make_sparse_matrix.py
@@ -12,26 +12,17 @@ movie_index = json.load(open('works/defs/smovie_index.json'))
 
 
 for TYPE in ['test', 'train']:
-    for index, fn in enumerate(glob.glob(f'./works/dataset/{TYPE}.csv')):
-        df = pd.read_csv(fn)
-        userIds = df['userId'].unique()
-        print(len(userIds))
-
-        movies = Sparse((len(userIds), len(movie_index),),
-                           dtype=np.float)
-        users = Sparse((len(userIds), len(user_index),),
-                           dtype=np.float)
-        for masterIndex, (userId, subDf) in enumerate(df.groupby(by=['userId'])):
-            print(userId)
-            uindex = user_index[str(userId)]
-            users[masterIndex, uindex] = 1.0
-            for movieId, score in zip(subDf['movieId'].tolist(), subDf['score'].tolist()):
-                    mindex = movie_index[str(movieId)]
-                    movies[masterIndex, mindex] = float(score)
-        print('try to compress')
-        pickle.dump(movies, open(
-            f'works/dataset/{TYPE}_movies.pkl', 'wb'))
-        pickle.dump(users, open(
-            f'works/dataset/{TYPE}_users.pkl', 'wb'))
-        #save_npz(f'works/dataset/{TYPE}_movies.lil', movies)
-        #save_npz(f'works/dataset/{TYPE}_users.lil', users)
+    X = []
+    fn = f'./works/dataset/{TYPE}.csv'
+    df=pd.read_csv(fn)
+    userIds=df['userId'].unique()
+    print(len(userIds))
+    for masterIndex, (userId, subDf) in enumerate(df.groupby(by=['userId'])):
+        print(userId)
+        uindex=user_index[str(userId)]
+        for movieId, score in zip(subDf['movieId'].tolist(), subDf['score'].tolist()):
+            mindex=movie_index[str(movieId)]
+            X.append( [uindex, mindex, score] )
+    print('try to compress')
+    pickle.dump(X, open(
+        f'works/dataset/{TYPE}_triples.pkl', 'wb'))
--- 60_matrix_factorization.py
+++ 60_matrix_factorization.py
@@ -12,32 +12,31 @@ import torch.nn.functional as F
 
 
 class MF(nn.Module):
-    def __init__(self, input_size):
+    def __init__(self, input_items, input_users):
         super(MF, self).__init__()
-        self.l_a1 = nn.Linear(
-            in_features=input_size, out_features=1024, bias=True)
-        self.l_a2 = nn.Linear(
-            in_features=1024, out_features=512, bias=True)
-        self.l_a3 = nn.Linear(
-            in_features=512, out_features=512, bias=True)
 
-        self.l_b1 = nn.Linear(
-            in_features=512, out_features=512, bias=True)
+        self.l_b1 = nn.Embedding(num_embeddings=input_items, embedding_dim=512)
         self.l_b2 = nn.Linear(
-            in_features=512, out_features=1024, bias=True)
+            in_features=512, out_features=512, bias=True)
         self.l_b3 = nn.Linear(
-            in_features=1024, out_features=input_size, bias=True)
+            in_features=512, out_features=1, bias=True)
 
-    def encode_user(self, x):
-        x = F.relu(self.l_a1(x))
-        x = F.relu(self.l_a2(x))
-        x = F.relu(self.l_a3(x))
-        return x
+        self.l_a1 = nn.Embedding(num_embeddings=input_users, embedding_dim=512)
+        self.l_a2 = nn.Linear(
+            in_features=512, out_features=512, bias=True)
+        self.l_a3 = nn.Linear(
+            in_features=512, out_features=1, bias=True)
 
     def encode_item(self, x):
         x = F.relu(self.l_b1(x))
         x = F.relu(self.l_b2(x))
         x = F.relu(self.l_b3(x))
+        return x
+
+    def encode_user(self, x):
+        x = F.relu(self.l_a1(x))
+        x = F.relu(self.l_a2(x))
+        x = F.relu(self.l_a3(x))
         return x
 
     def decode(self, x):
@@ -46,11 +45,18 @@ class MF(nn.Module):
         x = F.relu(self.l6(x))
         return x
 
-    def forward(self, item, user):
-        item_vec = self.encode(item)
-        user_vec = self.encode(user)
-        doted = item_vec.dot(user_vec)
+    def forward(self, inputs):
+        item_vec, user_vec = inputs
+        item_vec = self.encode_item(item_vec)
+        batch_size = list(item_vec.size())[0]
+
+        user_vec = self.encode_user(user_vec)
+        # print(user_vec.size())
+        doted = torch.bmm(user_vec.view(batch_size, 1, 1),
+                          item_vec.view(batch_size, 1, 17770))
+        doted = doted.view(batch_size, 17770)
         print(doted.size())
+        # exit()
         return doted
 
 
@@ -59,21 +65,31 @@ def myLoss(output, target):
     loss = torch.sqrt(torch.mean((output-target)**2))
     return loss
 
+
 def generate():
-  train_movies = pickle.load(open('works/dataset/train_movies.pkl', 'rb')).todense()
-  train_users  = pickle.load(open('works/dataset/train_users.pkl', 'rb')).todense() 
-  
-  for i in range(0, len(train_users), step=32):
-    yield (train_movies[i:i+32], train_users[i:i+32])
+    train_movies = pickle.load(open('works/dataset/train_movies.pkl', 'rb'))
+    train_users = pickle.load(open('works/dataset/train_users.pkl', 'rb'))
+    print(train_users.shape)
+
+    BATCH = 4
+    for i in range(0, train_users.shape[0], BATCH):
+        yield (train_movies[i:i+BATCH], train_users[i:i+BATCH])
+
 
 if __name__ == '__main__':
     movie_index = json.load(open('./works/defs/smovie_index.json'))
-    model = MF(len(movie_index)).to('cuda')
+    user_index = json.load(open('./works/defs/user_index.json'))
+    print(len(user_index))
+    model = MF(len(movie_index), len(user_index)).to('cuda')
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
 
-    for index, (train_movies, train_users) in generate():
-        inputs_movie = Variable(torch.from_numpy(train_movies)).float().cuda()
-        inputs_user = Variable(torch.from_numpy(train_users)).float().cuda()
+    for index, (train_movies, train_users) in enumerate(generate()):
+        print(train_movies.shape)
+        print(train_users.shape)
+        inputs_movie = Variable(torch.from_numpy(
+            train_movies.todense())).float().cuda()
+        inputs_user = Variable(torch.from_numpy(
+            train_users.todense())).float().cuda()
         predict = model([inputs_movie, inputs_user])
 
         loss = myLoss(predict, inputs_movie)
